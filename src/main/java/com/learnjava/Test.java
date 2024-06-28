2a5486b97e88b2718f83b83f3580c624c74e2fac
Swapnil-197 <sspatil1907@gmail.com>
2024-06-28 18:21:37 +0530

    changes done

--- src/main/java/com/learnjava/Test.java
+++ src/main/java/com/learnjava/Test.java
@@ -2,10 +2,10 @@ package com.learnjava;
 
 public class Test {
 	
-	public void add() {
+	public int  add() {
 		int a = 10 ;
 		int b = 20 ;
-		System.out.println(a+b);
+		return a+b;
 	}
 
 	public static void main(String[] args) {
