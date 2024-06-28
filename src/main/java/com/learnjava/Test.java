33a34175b27109c8aa49b0e39789712afdbd4aa2
Swapnil-197 <sspatil1907@gmail.com>
2024-06-28 17:55:22 +0530

    sub method added

--- src/main/java/com/learnjava/Test.java
+++ src/main/java/com/learnjava/Test.java
@@ -7,6 +7,12 @@ public class Test {
 		int b = 20 ;
 		System.out.println(a+b);
 	}
+	
+	public void sub() {
+		int a = 50 ;
+		int b = 20 ;
+		System.out.println(a-b);
+	}
 
 	public static void main(String[] args) {
 		
